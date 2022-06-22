5cdac77f22c9244f3edad00aa04a3c4da2faa1a1
Christopher Babb <chris@appsoftdev.com>
2022-06-22 14:49:24 -0700

    Game Board loading

--- script.js
+++ script.js
@@ -1,78 +1,158 @@
 const gameBoard = document.getElementById('game-container')
+const testGameBoard = "000000001000060020901000000710000005000000403000000700000000089000478000060000070"
+
+function createGameBoard() {
+    for(let i=0; i<9; i++){
+        for(let j=0; j<9; j++){
+            gameDIV = document.createElement('div')
+            gameDIV.setAttribute('id', `${i+1}-${j+1}`)
+            gameDIV.setAttribute('class', 'board-tile')
+            gameDIV.setAttribute('tileData', testGameBoard[(i*9)+j])
+
+            let populateTile = gameDIV.getAttribute('tileData')
+            
+            if(populateTile != '0'){
+                gameDIV.style.backgroundImage = `url('./images/${populateTile}_gameboard.png')`
+            }
+
+
+            if(i == 0){
+                gameDIV.style.top = '174px'
+            }
+            else if(i == 1){
+                gameDIV.style.top = '237px'
+            }
+            else if(i == 2){
+                gameDIV.style.top = '302px'
+            }
+            else if(i == 3){
+                gameDIV.style.top = '375px'
+            }
+            else if(i == 4){
+                gameDIV.style.top = '437px'
+            }
+            else if(i == 5){
+                gameDIV.style.top = '503px'
+            }
+            else if(i == 6){
+                gameDIV.style.top = '578px'
+            }
+            else if(i == 7){
+                gameDIV.style.top = '640px'
+            }
+            else{
+                gameDIV.style.top = '706px'
+            }
+            
+            if(j == 0){
+                gameDIV.style.marginRight = '270px'
+            }
+            else if(j == 1){
+                gameDIV.style.marginRight = '206px'
+            }
+            else if(j == 2){
+                gameDIV.style.marginRight = '142px'
+            }
+            else if(j == 3){
+                gameDIV.style.marginRight = '66px'
+            }
+            else if(j == 4){
+                gameDIV.style.marginRight = '2px'
+            }
+            else if(j == 5){
+                gameDIV.style.marginRight = '-65px'
+            }
+            else if(j == 6){
+                gameDIV.style.marginRight = '-139px'
+            }
+            else if(j == 7){
+                gameDIV.style.marginRight = '-204px'
+            }
+            else{
+                gameDIV.style.marginRight = '-268px'
+            }                
+
+            gameBoard.append(gameDIV)
+        }
+    }
+}
+
+function startGame() {
+    const gameContainer = document.getElementById('game-container')
+    gameContainer.style.backgroundImage = 'url("./images/blank_game_board.png")'
+
+    createGameBoard()
+}
 
 function mainMenu() {
-    backgroundEffects = document.createElement('img')
+    const backgroundEffects = document.createElement('img')
+    backgroundEffects.setAttribute('id', 'glowy-stuff')
     backgroundEffects.src = './images/glowy_stuff.png'
-    backgroundEffects.style.height = '860px'
-    backgroundEffects.style.position = 'absolute'
-    backgroundEffects.style.top = '0px'
-    backgroundEffects.style.opacity = '0'
-    backgroundEffects.style.animation = 'fadeIn 1s ease-out 1s 1 forwards';
     gameBoard.append(backgroundEffects)
 
-    menuBoard = document.createElement('img')
+    const menuBoard = document.createElement('img')
+    menuBoard.setAttribute('id', 'menu-background')
     menuBoard.src = './images/menu_background.png'
-    menuBoard.style.height = '815px'
-    menuBoard.style.position = 'absolute'
-    menuBoard.style.top = '0px'
-    menuBoard.style.marginLeft = '20px'
-    menuBoard.style.animation = '1s ease-out 0s 1 slideInFromTop';
     gameBoard.append(menuBoard)
 
-    menuTitle = document.createElement('img')
-    menuTitle.src = './images/game_title.png'
-    menuTitle.style.height = '125px'
-    menuTitle.style.position = 'absolute'
-    menuTitle.style.top = '90px'
-    menuTitle.style.right = '50%'
-    menuTitle.style.transform = 'translate(50%, 0px)'
-    menuTitle.style.opacity = '0'
-    menuTitle.style.animation = 'fadeIn 1s ease-out 1s 1 forwards';
-    gameBoard.append(menuTitle)
+    const gameMainMenuTitle = document.createElement('img')
+    gameMainMenuTitle.setAttribute('id', 'game-main-menu-title')
+    gameMainMenuTitle.src = './images/game_title.png'
+    gameBoard.append(gameMainMenuTitle)
 
-
-    birdGraphic = document.createElement('img')
+    const birdGraphic = document.createElement('img')
+    birdGraphic.setAttribute('id', 'bird-character')
     birdGraphic.src = './images/bird_character.png'
-    birdGraphic.style.height = '300px'
-    birdGraphic.style.position = 'relative'
-    birdGraphic.style.top = '425px'
-    birdGraphic.style.right = '50px'
     gameBoard.append(birdGraphic)
 
-    startGameButton = document.createElement('img')
+    const startGameButton = document.createElement('img')
+    startGameButton.setAttribute('id', 'start-game-button')
     startGameButton.src = './images/start_game_button.png'
-    startGameButton.style.height = '75px'
-    startGameButton.style.position = 'absolute'
-    startGameButton.style.top = '325px'
-    startGameButton.style.right = '50%'
-    startGameButton.style.transform = 'translate(50%, 0px)'
-    startGameButton.style.opacity = '0'
-    startGameButton.style.animation = 'fadeIn 1s ease-out 1s 1 forwards';
     gameBoard.append(startGameButton)
 
-    menuTitleShadow = document.createElement('h1')
+    const menuTitleShadow = document.createElement('h1')
+    menuTitleShadow.setAttribute('id', 'menu-title-shadow')
     menuTitleShadow.textContent = 'Main Menu'
-    menuTitleShadow.style.color = '#d8b366'
-    menuTitleShadow.style.fontSize = '46px'
-    menuTitleShadow.style.position = 'absolute'
-    menuTitleShadow.style.right = '50%'
-    menuTitleShadow.style.transform = 'translate(50%, 0px)'  
-    menuTitleShadow.style.top = '200px'
-    menuTitleShadow.style.opacity = '0'
-    menuTitleShadow.style.animation = 'fadeIn 1s ease-out 1s 1 forwards';
     gameBoard.append(menuTitleShadow)
 
-    menuTitle = document.createElement('h1')
+    const menuTitle = document.createElement('h1')
+    menuTitle.setAttribute('id', 'menu-title')
     menuTitle.textContent = 'Main Menu'
-    menuTitle.style.color = '#69251c'
-    menuTitle.style.fontSize = '46px'
-    menuTitle.style.position = 'absolute'
-    menuTitle.style.right = '50%'
-    menuTitle.style.transform = 'translate(50%, 0px)'
-    menuTitle.style.top = '195px'
-    menuTitle.style.opacity = '0'
-    menuTitle.style.animation = 'fadeIn 1s ease-out 1s 1 forwards';
     gameBoard.append(menuTitle)
+
+    //Add click event to Start button
+    const mainMenuStartButton = document.getElementById('start-game-button')
+    mainMenuStartButton.addEventListener('click', () => {
+        const mainMenuStartButton = document.getElementById('start-game-button')
+        const menuTitleShadow = document.getElementById('menu-title-shadow')
+        const menuTitle = document.getElementById('menu-title')
+        const glowyStuff = document.getElementById('glowy-stuff')
+        const menuBoard = document.getElementById('menu-background')
+        const birdCharacter = document.getElementById('bird-character')
+        const gameMainMenuTitle = document.getElementById('game-main-menu-title')
+
+        mainMenuStartButton.style.opacity = '1'
+        mainMenuStartButton.style.animation = 'fadeOut 1s ease-out 0s 1 forwards'
+
+        menuTitleShadow.style.opacity = '1'
+        menuTitleShadow.style.animation = 'fadeOut 1s ease-out 0s 1 forwards'
+
+        menuTitle.style.opacity = '1'
+        menuTitle.style.animation = 'fadeOut 1s ease-out 0s 1 forwards'
+
+        glowyStuff.style.opacity = '1'
+        glowyStuff.style.animation = 'fadeOut 1s ease-out 0s 1 forwards'
+
+        birdCharacter.style.opacity = '1'
+        birdCharacter.style.animation = 'fadeOut 1s ease-out 0s 1 forwards'
+
+        gameMainMenuTitle.style.opacity = '1'
+        gameMainMenuTitle.style.animation = 'fadeOut 1s ease-out 0s 1 forwards'        
+
+        menuBoard.style.animation = 'slideOutToTop 0.5s ease-out 1s 1 forwards'
+        
+        startGame()
+    })
 }
 
 mainMenu()
